f4e0117eb03654e22b0edba9cbe348afa52b13f7
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2020-04-12 14:20:46 +0530

    Added padding top for HomePage accrodian component to not shift while opening the accordians

--- src/components/HomePage/styles.tsx
+++ src/components/HomePage/styles.tsx
@@ -1,12 +1,13 @@
-import styled from 'styled-components';
-import { Accordion } from 'semantic-ui-react';
+import styled from "styled-components";
+import { Accordion } from "semantic-ui-react";
 
 export const HomePageStyled = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   flex-direction: column;
   height: 100vh;
+  padding-top: 15rem;
 
   /* source: http://www.brandgradients.com/slack-colors/ */
   background: linear-gradient(-120deg, #3eb991, #6ecadc, #e9a820, #e01563);
