814aed1c161d6fb8174cf85a636dbe7d04170777
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2020-05-29 22:06:14 +0530

    fix: remove Accordian styles

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,17 +1,15 @@
-import React, { useState } from 'react';
-import { CreateWorkspaceForm } from './CreateWorkspaceForm';
-import { JoinWorkspaceForm } from './JoinWorkspaceForm';
-import { HomePageStyled, AccordianStyled } from './styles';
+import React, { useState } from "react";
+import { CreateWorkspaceForm } from "./CreateWorkspaceForm";
+import { JoinWorkspaceForm } from "./JoinWorkspaceForm";
+import { HomePageStyled } from "./styles";
 
 export const HomePage: React.FC = () => {
   const [activeIndex, setIndex] = useState<number>(-1);
   return (
     <HomePageStyled>
-      <h1 style={{ textAlign: 'center' }}>Slack Clone</h1>
-      <AccordianStyled styled>
-        <JoinWorkspaceForm activeIndex={activeIndex} setIndex={setIndex} />
-        <CreateWorkspaceForm activeIndex={activeIndex} setIndex={setIndex} />
-      </AccordianStyled>
+      <h1 style={{ textAlign: "center" }}>Slack Clone</h1>
+      <JoinWorkspaceForm activeIndex={activeIndex} setIndex={setIndex} />
+      <CreateWorkspaceForm activeIndex={activeIndex} setIndex={setIndex} />
     </HomePageStyled>
   );
 };
--- src/components/HomePage/styles.tsx
+++ src/components/HomePage/styles.tsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { Accordion } from "semantic-ui-react";
 
 export const HomePageStyled = styled.div`
   display: flex;
@@ -11,8 +10,4 @@ export const HomePageStyled = styled.div`
 
   /* source: http://www.brandgradients.com/slack-colors/ */
   background: linear-gradient(-120deg, #3eb991, #6ecadc, #e9a820, #e01563);
-`;
-
-export const AccordianStyled = styled(Accordion)`
-  box-shadow: none !important;
 `;
