53c21b970d66dc8efae8b7b3a9e189ca2d03e0d9
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2020-05-29 22:12:54 +0530

    fix: Accordian from semantic-ui-react

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
-import { CreateWorkspaceForm } from "./CreateWorkspaceForm";
-import { JoinWorkspaceForm } from "./JoinWorkspaceForm";
-import { HomePageStyled } from "./styles";
+import React, { useState } from 'react';
+import { CreateWorkspaceForm } from './CreateWorkspaceForm';
+import { JoinWorkspaceForm } from './JoinWorkspaceForm';
+import { HomePageStyled } from './styles';
+import { Accordion } from 'semantic-ui-react';
 
 export const HomePage: React.FC = () => {
   const [activeIndex, setIndex] = useState<number>(-1);
   return (
     <HomePageStyled>
-      <h1 style={{ textAlign: "center" }}>Slack Clone</h1>
-      <JoinWorkspaceForm activeIndex={activeIndex} setIndex={setIndex} />
-      <CreateWorkspaceForm activeIndex={activeIndex} setIndex={setIndex} />
+      <h1 style={{ textAlign: 'center' }}>Slack Clone</h1>
+      <Accordion styled>
+        <JoinWorkspaceForm activeIndex={activeIndex} setIndex={setIndex} />
+        <CreateWorkspaceForm activeIndex={activeIndex} setIndex={setIndex} />
+      </Accordion>
     </HomePageStyled>
   );
 };
