8860877de3d7430f8aed169284b7a94ee25830d8
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2020-05-30 12:17:20 +0530

    style: Root page styling

--- src/components/HomePage/styles.tsx
+++ src/components/HomePage/styles.tsx
@@ -1,13 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const HomePageStyled = styled.div`
   display: flex;
-  justify-content: flex-start;
+  justify-content: center;
   align-items: center;
   flex-direction: column;
-  height: 100vh;
-  padding-top: 15rem;
-
-  /* source: http://www.brandgradients.com/slack-colors/ */
-  background: linear-gradient(-120deg, #3eb991, #6ecadc, #e9a820, #e01563);
+  height: 100%;
 `;
